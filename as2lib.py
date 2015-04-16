9f8a3d11203ec16034e1dab87505099a0e6d4628
Abhishek Ram <abhishek8816@gmail.com>
2015-04-16 11:35:05 +0530

    Fixed mic calc for encrypt only messages

--- as2lib.py
+++ as2lib.py
@@ -251,7 +251,7 @@ def build_message(message):
         content = as2utils.extractpayload(multipart).replace('\n','\r\n')
         payload = multipart
     if message.partner.encryption: 
-        if not message.signed and message.compressed:
+        if not message.signed and not message.compressed:
             micContent = as2utils.mimetostring(payload, 0).replace('\n','\r\n')
         models.Log.objects.create(message=message, status='S', text=_(u'Encrypting the message using partner key %s'%message.partner.encryption_key))
         message.encrypted = True
